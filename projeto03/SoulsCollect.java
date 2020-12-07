34fe7b4dc423d7cf5682fcc3b7f506aa099d99ba
Nasalis <davidossantosfreitas@hotmail.com>
2020-12-07 20:55:21 -0300

    Atualização no projeto SoulsCollect

--- projeto03/SoulsCollect.java
+++ projeto03/SoulsCollect.java
@@ -1,18 +1,20 @@
 package projeto03;
 
+import java.util.Random;
 import java.util.Scanner;
 
 class EnemySouls {
+
     int amountSouls;
     String name;
     String natureSouls;
     String infoAbout;
 
-    EnemySouls(int amountSouls, String natureSouls) {
+    EnemySouls(int amountSouls, String name, String natureSouls, String infoAbout) {
         this.amountSouls = amountSouls;
         this.natureSouls = natureSouls;
-        this.name = "Nahru Barshkman";
-        this.infoAbout = "'Nos interiores do submundo das Cavernas de Ighorah jazem as mais bestiais criaturas abandonadas pelos Criadores,\n aquelas cujo sangue enfraquece a terra e as águas e perturba a consciência dos homens tais como os pesadelos que atormentam os que vagam noturnamente pelas mentes.\nDe almas robustas e algoz da Criação, nenhuma mente sã as desejam vagando pelo mundo'\n--- Heroghito, Historiador, 457 a.C..\n";
+        this.name = name;
+        this.infoAbout = infoAbout;
     }
 
     void soulsSearched() {
@@ -31,12 +33,6 @@ class Collector {
     int leftovers;
     String typeSouls = "demon";
 
-    // void info(int maxSouls, String typeSouls) {
-    //     this.numSouls = 0;
-    //     this.leftovers = 0;
-    //     this.maxSouls = 200;
-    //     this.typeSouls = "demon";
-    // }
 
     void identifySouls() {
         if(soulsCollected != null) {
@@ -50,13 +46,13 @@ class Collector {
         if(this.soulsCollected != null) {
             System.out.println("That souls even collected.");
             return false;
-        }else if(!typeSouls.equals(target.natureSouls)) {
-            System.out.println("Hm... Are you sure that you prepared to collect this type souls, Collector?\nBecause I suppose the opposite...\n");
-            return false;
+        }else if(target.natureSouls.equals(typeSouls)) {
+            System.out.println("Souls collected");
+            soulsCollected = target;
+            return true;
         }
-        System.out.println("Souls collected");
-        soulsCollected = target;
-        return true;
+        System.out.println("Hm... Are you sure that you prepared to collect this type souls, Collector?\nBecause I suppose the opposite...\n");
+        return false;
     }
 
     void absorbSouls() {
@@ -75,19 +71,36 @@ class Collector {
     }
 
     public String toString() {
-        return "Amount souls " + numSouls + "/" + maxSouls + "\nLeftovers: " + leftovers + "\nType Collector: " + typeSouls + "\nSouls: " + soulsCollected;
+        return "Amount souls " + numSouls + "/" + maxSouls + "\nLeftovers: " + leftovers + "\nType Collector: " + typeSouls + "\n";
     }
 }
 
 public class SoulsCollect {
     public static void main(String[] args) {
         Collector souls = new Collector();
-        EnemySouls demon = new EnemySouls(20, "demon");
         Scanner input = new Scanner(System.in);
+
+       
+        SoulsList soulslist[] = new SoulsList[5];
+        soulslist[0] = new SoulsList(0, "Nenhuma alma encontrada", "Vazio", "Vazio");
+        soulslist[1] = new SoulsList(20, "Nahru Barshkman", "demon", "'Nos interiores do submundo das Cavernas de Ighorah jazem as mais bestiais criaturas abandonadas pelos Criadores,\naquelas cujo sangue enfraquece a terra e as águas e perturba a consciência dos homens tais como os pesadelos que atormentam os que vagam noturnamente pelas mentes.\nDe almas robustas e algoz da Criação, nenhuma mente sã as desejam vagando pelo mundo'\n--- Heroghito, Historiador, 457 a.C..\n");
+        soulslist[2] = new SoulsList(72, "Weretolfus", "licantropo", "'Os antigos acreditavam que durante o alvorecer nasciam os homens: saudáveis e agéis, devotos e leais ao seu povo,\ne durante os vislumbres noturnos, aqueles que derramam lágrimas após a luz do parto, pouco restam de humanidade, pois esta fora tomada pela malícia do mundo.\nAssim resta-lhes uivar a noite, caçar as presas, abandonar os humanos e viver na solidão quando a lua banha o céu\n--- Perclarius, Historiador, 398 a.C..'");
+        soulslist[3] = new SoulsList(55, "Squirin", "centauro", "'De postura robusta, força e destreza icomensurável os centauros dominam as florestas e vasculham os campos em vigilância com maior primor do que qualquer outro soldado extramamente qualificado em sua melhor performance. Quando desermados, pisoteiam e desfiguram o rosto do alvo; quando seguram seus arcos, causam pavor; com as lanças, uma vitória na guerra'\n--- Casius, general militar, 257 a.C..");
+        soulslist[4] = new SoulsList(100, "Kashimaren", "baku", "'Nem animal nem homem, tampouco compreensível aos primeiros olhares, seja à distância ou próximo de qualquer visão, jamais compreenderei tamanha imagem mitológica.\nDigo assim não por sua elegância ou carga de poder, mas pela atrocidade de acreditar que, de fato, a Criação a fez na Terra'\n--- Heramus, Sacerdote, 320 a.C..");
 
         while(true) {
             String line = input.nextLine();
             String[] command = line.split(" ");
+
+            Random number = new Random();
+            int soulsSelected = number.nextInt(5);
+
+            int qtdSouls = soulslist[soulsSelected].getAmountSouls();
+            String name = soulslist[soulsSelected].getName();
+            String nature = soulslist[soulsSelected].getNatureSouls();
+            String info = soulslist[soulsSelected].getInfoAbout();
+
+            EnemySouls demon = new EnemySouls(qtdSouls, name, nature, info);
 
             if(command[0].equals("identify")) {
                 souls.identifySouls();
