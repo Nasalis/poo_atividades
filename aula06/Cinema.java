5bb65d0b13d109f28ff3093cb4d8254aedc6efed
Nasalis <davidossantosfreitas@hotmail.com>
2021-01-11 12:31:37 -0300

    atividade finalizada

--- aula06/Cinema.java
+++ aula06/Cinema.java
@@ -27,29 +27,44 @@ public class Cinema {
     }
 
     void reservar(Cliente cliente, int indice) {
-        if(list.get(indice) == null) {
+        if(clientes.get(indice).id.equals("")) {
             clientes.set(indice, cliente);
+        }else if(clientes.get(indice).id.equals(cliente.id)){
+            System.out.println("fail: cliente ja esta no cinema");
+        }else if(!clientes.get(indice).id.equals("")) {
+            System.out.println("fail: cadeira ja esta ocupada");
         }
     }
 
     void cancelar(String nome) {
 
-        for(Cliente cliente : clientes) {
-            if(cliente.id.equals(nome)) {
-                int index = clientes.indexOf(cliente);
-                clientes.remove(index);
+        for(int i = 0; i < clientes.size(); i++) {
+            if(clientes.get(i).id.equals(nome)) {
+                clientes.remove(i);
+                System.out.println(nome + " saiu");
             }
         }
 
     }
 
+    void showInfo(ArrayList<Cliente> clientes) {
+        System.out.print("[ ");
+        for(int i = 0; i < clientes.size(); i++) {
+            if(clientes.get(i).id.equals("")) {
+                System.out.print(" -");
+            }else{
+                System.out.print(clientes.get(i).id + ":" + clientes.get(i).fone + " ");
+            }
+        }
+        System.out.print(" ]\n");
+    }
+ 
     public String toString() {
         String infoCadeiras = "";
 
-    
         for(int i = 0; i < tamanho; i++) {
             if(list.get(i) == null) {
-                clientes.add(null);
+                clientes.add(new Cliente("",""));
             }
         }
 
@@ -63,37 +78,13 @@ public class Cinema {
         Cinema cinema = new Cinema(5);
         System.out.println(cinema.list);
         System.out.println(cinema);
-        System.out.println(cinema.clientes);
         cinema.reservar(new Cliente("davi", "3232"), 0);
         cinema.reservar(new Cliente("eliana", "3131"), 2);
         cinema.reservar(new Cliente("edvaldo", "4242"), 1);
-
-        System.out.print("[ ");
-        for(int i = 0; i < cinema.clientes.size(); i++) {
-            if(cinema.clientes.get(i) == null) {
-                System.out.print(" -");
-            }else{
-                System.out.print(cinema.clientes.get(i).id + ":" + cinema.clientes.get(i).fone + " ");
-            }
-        }
-        System.out.print(" ]\n");
-        
-        
-        System.out.println(cinema.clientes.indexOf(cinema.clientes.get(2)));
-        System.out.println(cinema.clientes.remove(2));
-
-        System.out.print("[ ");
-        for(int i = 0; i < cinema.clientes.size(); i++) {
-            if(cinema.clientes.get(i) == null) {
-                System.out.print(" -");
-            }else{
-                System.out.print(cinema.clientes.get(i).id + ":" + cinema.clientes.get(i).fone + " ");
-            }
-        }
-        System.out.print(" ]\n");
-
-        // System.out.println(cinema);
-
+        cinema.reservar(new Cliente("davi", "3232"), 0);
+        cinema.showInfo(cinema.clientes);
+        cinema.cancelar("davi");
+        cinema.showInfo(cinema.clientes);
 
     }
 }
